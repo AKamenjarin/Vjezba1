1ce79604c8c819d2626b2f057f8999d23150a738
AKamenjarin <antonia.kamenjarin@gmail.com>
2019-04-02 15:13:12 +0200

    second commit

--- vj1/Program.cs
+++ vj1/Program.cs
@@ -10,7 +10,7 @@ namespace vj1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("Hello World!");
         }
     }
 }
